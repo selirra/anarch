f45ece5a13c16ecc6428ec0c08778e30bcc4bce0
selirra <patecskotwo@gmail.com>
2021-06-01 21:49:59 +0200

    fffffffff

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,18 +1,28 @@
 settings_get_path()
 {
 	decoration_message _ "The installer will create a directory for your linux rootfs, enter it's name!"
-	decoration_text_centered "It will be located inside '/anarch'!"
+	decoration_text_centered "It will be located inside '/data/anarch'!"
 
-	read -p 'Enter a path: ' settings_installdir
+	read -p 'Enter your directory name: ' settings_installdir
 
-	settings_installpath="/anarch/"$settings_installdir
+	settings_installpath="/data/anarch/"$settings_installdir
 
 	if [ ! -d "$settings_installpath" ]
 	then
   		mkdir -p $settings_installpath
+
 		echo
-		decoration_line _
-		decoration_text_centered "$settings_installdir directory created in '/anarch'!"
+		decoration_message _ "$settings_installdir directory created in '/data/anarch'!"
+		echo
+	fi
+
+	if [ ! -L /anarch ]
+	then
+  		ln -sf /data/anarch /anarch 
+
+		echo
+		decoration_message _ "'/data/anarch' symlink created at your device's root directory!"
+		echo
 	fi
 
 	return
@@ -21,20 +31,21 @@ settings_get_path()
 settings_get_mountstorage()
 {
 	decoration_message _ "Do you want to mount your internal storage in the container?"
-	decoration_text_centered "(Yes / No)"
+	decoration_text_centered "(y / n)"
 
 	read -p 'Answer: ' settings_mountstorage
 
-	if [ $settings_mountstorage = "yes" ]
+	if [ $settings_mountstorage = "y" ]
 	then
 		return
-	elif [ $settings_mountstorage = "no" ]
+	elif [ $settings_mountstorage = "n" ]
 	then
 		return
 	else
 		echo
-		decoration_message ! "Invalid input, type \"yes\", or \"no\"." 
+		decoration_message ! "Invalid input, type 'y', or 'n'." 
 		echo
+
 		settings_get_mountstorage
 	fi
 }
@@ -45,25 +56,32 @@ settings_verification()
 	decoration_text_centered "Mount internal storage: $settings_mountstorage"
 	echo
 	decoration_message _ "Are these settings correct?"
-	decoration_text_centered "(Yes / No)"
+	decoration_text_centered "(y / n)"
 
 	read -p 'Answer: ' settings_verification_answer
 
-	if [ $settings_verification_answer = "yes" ]
+	if [ $settings_verification_answer = "y" ]
 	then
 		return
-	elif [ $settings_verification_answer = "no" ]
+	elif [ $settings_verification_answer = "n" ]
 	then
-		echo
-		decoration_line _
-		decoration_text_centered "Exiting installation..."
-
-        exit 1
+        settings_exit
 	else
 		echo
-		decoration_line !
-		decoration_text_centered "Invalid input, type \"yes\", or \"no\"."
+		decoration_message ! "Invalid input, type 'y', or 'n'."
 		echo
+
 		settings_verification
 	fi
+}
+
+settings_exit()
+{
+	echo
+	decoration_message _ "Exiting installation..."
+	echo
+
+	rmdir $settings_installpath
+
+	exit 1
 }
